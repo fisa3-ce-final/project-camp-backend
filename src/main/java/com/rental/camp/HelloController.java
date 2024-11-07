e0edc74016a22cbd0072d0afafdcfe7c17094bc9
recoild <cajhyung@gmail.com>
2024-11-08 02:33:17 +0900

    feat: cognito 권한 읽기 기능 추가. 함수 RBAC 기능 추가.

--- src/main/java/com/rental/camp/HelloController.java
+++ src/main/java/com/rental/camp/HelloController.java
@@ -1,15 +1,19 @@
 package com.rental.camp;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String user(@AuthenticationPrincipal OidcUser oidcUser) {
-        return "Hello, " + oidcUser.getEmail();
+    @PreAuthorize("hasRole('USER')")
+    public String user(JwtAuthenticationToken principal) {
+        String email = principal.getTokenAttributes().get("email").toString();
+        String picture = principal.getTokenAttributes().get("picture").toString();
+        String name = principal.getName();
+        return "Hello, " + email;
     }
 
     @GetMapping("/")
--- src/main/java/com/rental/camp/global/config/SecurityConfig.java
+++ src/main/java/com/rental/camp/global/config/SecurityConfig.java
@@ -3,21 +3,19 @@ package com.rental.camp.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
-    private final CustomizeAuthenticationSuccessHandler customizeAuthenticationSuccessHandler;
+    private final JwtAuthConverter jwtAuthConverter;
 
     private final String[] WHITE_LIST = {
             "/public/**",
@@ -25,58 +23,26 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/actuator/**",
-            "/login",
-            "/"
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .sessionManagement(session -> {
-//                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//                })
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers(WHITE_LIST).permitAll()
                         .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .oidcUserService(this.oidcUserService())
-                        )
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                );
+                .oauth2ResourceServer(oauth2 -> {
+                    oauth2.jwt(jwt -> {
+                        jwt.jwtAuthenticationConverter(jwtAuthConverter);
+                    });
+                });
         return http.build();
     }
 
-    @Bean
-    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        return new OidcUserService();
-    }
-
-//
-//    @Bean
-//    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
-//        return (authorities) -> {
-//            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-//
-//            try {
-//                OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) new ArrayList<>(authorities).get(0);
-//
-//                mappedAuthorities = ((ArrayList<?>) oidcUserAuthority.getAttributes().get("cognito:groups")).stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(Collectors.toSet());
-//            } catch (Exception exception) {
-//                System.out.println("Not Authorized!");
-//
-//                System.out.println(exception.getMessage());
-//            }
-//
-//            return mappedAuthorities;
-//        };
-//    }
 }
