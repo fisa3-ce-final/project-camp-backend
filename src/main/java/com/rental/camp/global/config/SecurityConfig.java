0c9bdf50b4b6830bb974a4fda7864d504539353a
recoild <cajhyung@gmail.com>
2024-11-07 09:54:00 +0900

    feat: aws cognito 회원가입 기능 구현

--- src/main/java/com/rental/camp/global/config/SecurityConfig.java
+++ src/main/java/com/rental/camp/global/config/SecurityConfig.java
@@ -1,46 +1,82 @@
-//package com.rental.camp.global.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@RequiredArgsConstructor
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig {
-//    private final JwtAuthConverter jwtAuthConverter;
-//
-//    private final String[] WHITE_LIST = {
-//            "/public/**",
-//            "/swagger-ui/**",
-//            "/v3/api-docs/**",
-//            "/swagger-resources/**",
-//            "/actuator/**",
-//    };
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
+package com.rental.camp.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final CustomizeAuthenticationSuccessHandler customizeAuthenticationSuccessHandler;
+
+    private final String[] WHITE_LIST = {
+            "/public/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/actuator/**",
+            "/login",
+            "/"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
 //                .sessionManagement(session -> {
 //                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //                })
-//                .formLogin(AbstractHttpConfigurer::disable)
-//                .httpBasic(AbstractHttpConfigurer::disable)
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(req -> req
-//                        .requestMatchers(WHITE_LIST).permitAll()
-//                        .anyRequest().authenticated())
-//                .oauth2ResourceServer(oauth2 -> {
-//                    oauth2.jwt(jwt -> {
-//                        jwt.jwtAuthenticationConverter(jwtAuthConverter);
-//                    });
-//                });
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .oidcUserService(this.oidcUserService())
+                        )
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                );
+        return http.build();
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return new OidcUserService();
+    }
+
 //
-//        return http.build();
+//    @Bean
+//    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+//        return (authorities) -> {
+//            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+//
+//            try {
+//                OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) new ArrayList<>(authorities).get(0);
+//
+//                mappedAuthorities = ((ArrayList<?>) oidcUserAuthority.getAttributes().get("cognito:groups")).stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(Collectors.toSet());
+//            } catch (Exception exception) {
+//                System.out.println("Not Authorized!");
+//
+//                System.out.println(exception.getMessage());
+//            }
+//
+//            return mappedAuthorities;
+//        };
 //    }
-//}
+}
