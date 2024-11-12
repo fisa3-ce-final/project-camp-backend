a56ea8899de7b66725f01ccd18e47fb562bd4f99
recoild <cajhyung@gmail.com>
2024-11-12 16:03:53 +0900

    feat: User 수정, 삭제 기능

--- src/main/java/com/rental/camp/user/controller/UserController.java
+++ src/main/java/com/rental/camp/user/controller/UserController.java
@@ -1,24 +1,65 @@
 package com.rental.camp.user.controller;
 
+import com.rental.camp.user.dto.UserGetResponse;
+import com.rental.camp.user.dto.UserModifyRequest;
+import com.rental.camp.user.dto.UserModifyResponse;
 import com.rental.camp.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/signin")
+    @PostMapping
     public ResponseEntity<String> signin(JwtAuthenticationToken principal) {
-        String uuid = principal.getName();
-        userService.signIn(uuid);
+        if (principal == null) {
+            return ResponseEntity.badRequest().body("signin failed");
+        }
+        userService.signIn(principal);
+        return ResponseEntity.ok("signin success: " + principal.getName());
+    }
 
-        return ResponseEntity.ok("signin success: " + uuid);
+    @GetMapping
+    public ResponseEntity<UserGetResponse> me(JwtAuthenticationToken principal) {
+        if (principal == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        UserGetResponse userGetResponse = userService.getUser(principal.getName());
+        if (userGetResponse == null) {
+            return ResponseEntity.ok(null);
+        }
+
+        return ResponseEntity.ok(userGetResponse);
+    }
+
+    @PutMapping
+    public ResponseEntity<UserModifyResponse> updateUser(@ModelAttribute UserModifyRequest userModifyRequest,
+                                                         JwtAuthenticationToken principal) {
+        if (principal == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        UserModifyResponse res = userService.updateUser(principal, userModifyRequest);
+        return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> signout(JwtAuthenticationToken principal) {
+        if (principal == null) {
+            return ResponseEntity.badRequest().body("signout failed");
+        }
+        userService.deleteUser(principal);
+        return ResponseEntity.ok("delete success: " + principal.getName());
     }
 }
--- src/main/java/com/rental/camp/user/service/UserServiceImpl.java
+++ src/main/java/com/rental/camp/user/service/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.rental.camp.user.service;
 
+import com.rental.camp.user.dto.UserGetResponse;
+import com.rental.camp.user.dto.UserModifyRequest;
+import com.rental.camp.user.dto.UserModifyResponse;
 import com.rental.camp.user.model.User;
 import com.rental.camp.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,12 +18,20 @@ public class UserServiceImpl implements UserService {
     final UserRepository userRepository;
 
     @Override
-    public void signIn(String _uuid) {
-        UUID uuid = UUID.fromString(_uuid);
+    public void signIn(JwtAuthenticationToken principal) {
+        UUID uuid = UUID.fromString(principal.getName());
+        String email = principal.getTokenAttributes().get("email").toString();
+        String picture = principal.getTokenAttributes().get("picture").toString();
         User exsistingUser = userRepository.findByUuid(uuid);
         if (exsistingUser == null) {
             User newUser = User.builder()
                     .uuid(uuid)
+                    .email(email)
+                    .nickname("닉네임_" + uuid.toString().split("-")[0])
+                    .phone("")
+                    .address("")
+                    .imageUrl(picture)
+                    .isDeleted(false)
                     .build();
             userRepository.save(newUser);
         } else {
@@ -27,6 +39,57 @@ public class UserServiceImpl implements UserService {
 
             exsistingUser.setUpdatedAt(date);
             userRepository.save(exsistingUser);
+        }
+    }
+
+    @Override
+    public UserGetResponse getUser(String _uuid) {
+        UUID uuid = UUID.fromString(_uuid);
+        User user = userRepository.findByUuid(uuid);
+        if (user != null) {
+            return UserGetResponse.builder()
+                    .uuid(user.getUuid())
+                    .username(user.getUsername())
+                    .email(user.getEmail())
+                    .phone(user.getPhone())
+                    .address(user.getAddress())
+                    .nickname(user.getNickname())
+                    .imageUrl(user.getImageUrl())
+                    .provider(user.getProvider())
+                    .createdAt(user.getCreatedAt())
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Override
+    public UserModifyResponse updateUser(JwtAuthenticationToken principal, UserModifyRequest userModifyRequest) {
+        UUID uuid = UUID.fromString(principal.getName());
+        User user = userRepository.findByUuid(uuid);
+        if (user != null) {
+            user.setPhone(userModifyRequest.getPhone());
+            user.setAddress(userModifyRequest.getAddress());
+            user.setNickname(userModifyRequest.getNickname());
+            userRepository.save(user);
+
+            return UserModifyResponse.builder()
+                    .phone(user.getPhone())
+                    .address(user.getAddress())
+                    .nickname(user.getNickname())
+                    .imageUrl(user.getImageUrl())
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteUser(JwtAuthenticationToken principal) {
+        UUID uuid = UUID.fromString(principal.getName());
+        User user = userRepository.findByUuid(uuid);
+        if (user != null) {
+            userRepository.delete(user);
         }
     }
 }
