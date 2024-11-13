769d09eae649493d2ab2f6d7413c7c49d503119b
recoild <cajhyung@gmail.com>
2024-11-13 09:02:49 +0900

    feat: s3 client 추가

--- src/main/java/com/rental/camp/user/service/UserService.java
+++ src/main/java/com/rental/camp/user/service/UserService.java
@@ -5,12 +5,14 @@ import com.rental.camp.user.dto.UserModifyRequest;
 import com.rental.camp.user.dto.UserModifyResponse;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.io.IOException;
+
 public interface UserService {
     void signIn(JwtAuthenticationToken principal);
 
     UserGetResponse getUser(String _uuid);
 
-    UserModifyResponse updateUser(JwtAuthenticationToken principal, UserModifyRequest userModifyRequest);
+    UserModifyResponse updateUser(JwtAuthenticationToken principal, UserModifyRequest userModifyRequest) throws IOException;
 
     void deleteUser(JwtAuthenticationToken principal);
 }
