46da0d9791951e0ce96bf98650be817e8737ea24
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2024-11-27 15:35:09 +0900

    feat: CORS url 수정

--- src/main/java/com/rental/camp/global/config/SecurityConfig.java
+++ src/main/java/com/rental/camp/global/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.recoild-toy.xyz")); // 허용 도메인
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.wooricamping.com")); // 허용 도메인
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
